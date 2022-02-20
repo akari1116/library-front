cb4e4ed7e1eaf45436f23b64640827741233a2ee
suzukiakari <kfsa_luv3.7@icloud.com>
2022-02-20 12:54:51 +0900

    プログラムコミット

---
+++ src/app/constant/Url.ts
@@ -0,0 +1,9 @@
+export class Url {
+
+    static readonly BACK_HOST = "http://localhost:8080/library/";
+    static readonly BACK_HOST_LOGIN = "http://localhost:8080/login";
+
+    static readonly routerUrl = {
+        "error" : "error"
+    }
+}
